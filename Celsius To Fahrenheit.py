046f21ea6b3b865aefc20a1f39a8c465d9a6d7b8
karansharma1710 <100886367+karansharma1710@users.noreply.github.com>
2022-05-27 00:31:31 +0530

    Temperature Convert

---
+++ Celsius To Fahrenheit.py
@@ -0,0 +1,3 @@
+celsius_2 = float (input("Temperature value in degree Celsius: " ))  
+Fahrenheit_2 = (celsius_2 * 9/5) + 32  
+print ('The %.2f degree Celsius is equal to: %.2f Fahrenheit'%(celsius_2, Fahrenheit_2)) 
